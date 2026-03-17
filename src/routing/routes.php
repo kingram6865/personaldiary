5fffd2cd48165fd52a70f2aca85455ca6b3e3eac
Ken Ingram <ken.ingram@gmail.com>
2026-03-16 18:03:36 -0700

    Routing used instead of static directory division.

---
+++ src/routing/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    '/personal/diary' => 'home',
+    '/personal/diary/new' => 'newEntry',
+    '/personal/diary/list' => 'listEntries',
+    '/personal/diary/search' => 'searchEntries',
+    '/personal/diary/title-search' => 'titleSearchEntries',
+];
